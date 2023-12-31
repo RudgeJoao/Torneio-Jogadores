0ceb76f1fb771a0f1559008776ad7ce41191656a
RudgeJoao <113056261+RudgeJoao@users.noreply.github.com>
2023-12-31 03:10:03 -0300

    [FEAT]: Web Api created. Ref. #2

---
+++ Torneio/TorneioApi/Repositories/PlayerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TorneioApi.Data;
+using TorneioApi.Domain;
+
+namespace TorneioApi.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly OracleDbContext _dbContext;
+
+        public PlayerRepository(OracleDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        } 
+        
+        public async Task<List<Player>> GetPlayers() 
+        {
+            var query = await _dbContext.Players.OrderBy(x=> x.Id).ToListAsync();
+            return query;
+        }
+    }
+}
